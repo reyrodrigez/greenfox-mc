d73931cb85530082d2467bf0d97ca5db2f94d1b4
Ádám Gólya <adam.stork@gmail.com>
2017-02-01 18:18:10 +0100

    feat(server.js): add request monitor features to the webserver

--- src/server/container.js
+++ src/server/container.js
@@ -4,6 +4,7 @@ const path = require('path');
 const di = diTools.getDI();
 di.registerModule(require('lab-config'), 'config');
 di.registerModule(require('lab-config/implementations/memory'), 'config-memory');
+di.registerModule(require('lab-config/implementations/file'), 'config-file');
 
 diTools.registerDir(path.resolve(__dirname, 'external'));
 diTools.registerDir(path.resolve(__dirname, 'internal'));
--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,4 @@
-import di from './container';
+import container from './container';
 
 require('dotenv').config()
 const path = require('path');
@@ -9,6 +9,19 @@ const app = express();
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
+
+app.use(async (req, res, next) => {
+    const monitor = container.get('requestmonitor');
+    await monitor.registerIncomingRequest();
+    next();
+  }
+);
+
+app.use('/stats', async (req, res, next) => {
+  const monitor = container.get('requestmonitor');
+  const result = await monitor.getStatistics()
+  res.send(result);
+});
 
 app.use('/', express.static(path.join(__dirname, process.env.PUBLIC_DIR)))
 
