058a6b7c3ab2051ddd7f9ab5815a5b0c593fc3a8
Ádám Gólya <adam.stork@gmail.com>
2017-03-07 10:16:27 +0100

    refactor(recalculate.js): remove unnecessary calls

--- src/server/recalculate.js
+++ src/server/recalculate.js
@@ -3,16 +3,7 @@ import VError from 'verror';
 
 const requestMonitor = container.get('requestmonitor');
 
-async function recalculate() {
-  await requestMonitor.registerIncomingRequest('url');
-  await requestMonitor.registerIncomingRequest('url');
-  await requestMonitor.registerIncomingRequest('url');
-
-  await requestMonitor.recalculate();
-  console.log(await requestMonitor.getStatistics());
-}
-
-recalculate().then(()=>{
+requestMonitor.recalculate().then(()=>{
   console.log('recalculation was successful');
 }).catch((error)=>{
   console.log(new VError(error, `[Recalcuate] The recalcuation was unsuccessful`));
